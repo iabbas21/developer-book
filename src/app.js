362ba27fee8f35e8654f76c423c35c0b9a633d18
iabbas21 <i.irfanabbas1996@gmail.com>
2026-07-26 22:13:29 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,27 +2,27 @@ const express = require('express')
 
 const app = express()
 
-app.use('/user', (req, res) => {
-    res.send('Handle all HTTP methods of route /user')
-})
-
-app.get('/user', (req, res) => {
-    res.send({ firstName: 'Irfan', lastName: 'Abbas' })
-})
-
-app.post('/user', (req, res) => {
-    console.log('Save data in DB')
-    res.send("Data saved successfully!")
-})
-
-app.delete('/user', (req, res) => {
-    console.log('Delete data from DB')
-    res.send("Data deleted successfully!")
-})
-
-app.use('/', (req, res) => {
-    res.send('Hello from server')
-})
+app.get('/user', 
+    (req, res, next) => {
+        console.log('Route handler 1')
+        next()
+    }, 
+    (req, res, next) => {
+        console.log('Route handler 2')
+        // res.send('Route Handler 2')
+        next()
+    },
+    (req, res, next) => {
+        console.log('Route handler 3')
+        // res.send('Route Handler 2')
+        next()
+    },
+    (req, res, next) => {
+        console.log('Route handler 4')
+        res.send('Route Handler 4')
+        // next()
+    }
+)
 
 app.listen(3000, () => {
     console.log(`Server is running successfully`)
