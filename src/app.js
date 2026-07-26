5118ace15c4a1c167ff6277f61f5102230186e49
iabbas21 <i.irfanabbas1996@gmail.com>
2026-07-26 14:49:30 +0530

    exploring http methods of routes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,26 @@ const express = require('express')
 
 const app = express()
 
+app.use('/user', (req, res) => {
+    res.send('Handle all HTTP methods of route /user')
+})
+
+app.get('/user', (req, res) => {
+    res.send({ firstName: 'Irfan', lastName: 'Abbas' })
+})
+
+app.post('/user', (req, res) => {
+    console.log('Save data in DB')
+    res.send("Data saved successfully!")
+})
+
+app.delete('/user', (req, res) => {
+    console.log('Delete data from DB')
+    res.send("Data deleted successfully!")
+})
+
 app.use('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.use('/home', (req, res) => {
-    res.send('Namaste from dashboard')
-})
-
-app.use('/test', (req, res) => {
-    res.send('Hello from the server!')
+    res.send('Hello from server')
 })
 
 app.listen(3000, () => {
